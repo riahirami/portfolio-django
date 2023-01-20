60cf59293419b7e67d8f71098810c9d01ce3ba6b
ramos <riahirami91@gmail.com>
2023-01-20 22:52:31 +0100

    urls without index

--- projects/urls.py
+++ projects/urls.py
@@ -5,7 +5,7 @@ from projects import views
 urlpatterns = [  
     path('admin/', admin.site.urls),  
     path('show', views.project),  
-    path('index',views.show),  
+    path('',views.show),  
     path('edit/<int:id>', views.edit),  
     path('update/<int:id>', views.update),  
     path('delete/<int:id>', views.destroy),  
