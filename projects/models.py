401bd58e2245c766331ad0be358fe31d0195a634
ramos <riahirami91@gmail.com>
2023-01-22 17:40:48 +0100

    update db and model

--- projects/models.py
+++ projects/models.py
@@ -2,7 +2,7 @@ from django.db import models
 class Projects(models.Model):  
     id = models.AutoField(primary_key=True,auto_created = True) 
     name = models.CharField(max_length=100)  
-    # description = models.CharField(max_length=200)  
+    description = models.TextField(max_length=1000,null="True")  
     image = models.FileField(null="True",blank="True",upload_to="images/")   
     url = models.URLField(max_length=100) 
     techno = models.CharField(max_length=100) 
